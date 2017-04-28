a3162ba0559b11a77ebfb505e42610e1cebd7355
zhangxiaoping <zhangxiaoping91@163.com>
2017-04-28 14:37:48 +0800

    dubbo rpc框架

---
+++ dubbo-provider/src/main/java/cn/software/rpc/dubbo/service/IWelcomeService.java
@@ -0,0 +1,11 @@
+package cn.software.rpc.dubbo.service;
+
+/**
+ * @author ：zhangxpc@jiedaibao.com
+ * @Description ：   欢迎service
+ * @ClassName ：IWelcomeService
+ * @date ：2017/4/28 14:19
+ */
+public interface IWelcomeService {
+     void sayHello(String name);
+}
---
+++ dubbo-provider/src/main/java/cn/software/rpc/dubbo/service/impl/WelcomeServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.software.rpc.dubbo.service.impl;
+
+import cn.software.rpc.dubbo.service.IWelcomeService;
+
+/**
+ * @author ：zhangxpc@jiedaibao.com
+ * @Description ：
+ * @ClassName ：WelcomeServiceImpl
+ * @date ：2017/4/28 14:21
+ */
+public class WelcomeServiceImpl implements IWelcomeService {
+    public void sayHello(String name) {
+
+    }
+}
